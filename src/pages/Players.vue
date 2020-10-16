a53ed896e8d189fbb25d6803e51b3195f415808d
FoohonPie <674157+foohonpie@users.noreply.github.com>
2020-10-16 11:22:28 -0500

    adding dotenv quasar extension, .env.example, algolia results for Players page, updating .gitignore

--- src/pages/Players.vue
+++ src/pages/Players.vue
@@ -1,11 +1,38 @@
 <template>
-  <q-page padding>
-    <!-- content -->
+  <q-page padding class="q-mt-xl q-ml-xl">
+    <ais-index
+      :app-id="ALGOLIA_APP_ID"
+      :api-key="ALGOLIA_KEY"
+      index-name="users"
+    >
+      <ais-search-box placeholder="Search players..."></ais-search-box>
+      <ais-results>
+        <template slot-scope="{ result }">
+          <h2>
+            <a :href="result.path">
+              <h2>
+                <ais-highlight :result="result" attribute-name="name"></ais-highlight>
+              </h2>
+            </a>
+          </h2>
+          <p>
+            <ais-highlight :result="result" attribute-name="short_description"></ais-highlight>
+          </p>
+        </template>
+      </ais-results>
+      <ais-powered-by></ais-powered-by>
+    </ais-index>
   </q-page>
 </template>
 
 <script>
 export default {
-  // name: 'PageName',
+  name: 'PageIndex',
+  data () {
+    return {
+      ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
+      ALGOLIA_KEY: process.env.ALGOLIA_KEY
+    }
+  }
 }
 </script>
