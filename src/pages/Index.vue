43d7235d46d88cce8cd53b3b170ea92e4d6be187
Brian Kennedy <bpkennedy@gmail.com>
2020-11-13 10:08:23 -0600

    Create home page with styles, primary color

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,42 @@
+<script>
+export default {}
+</script>
+
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
+  <q-page class="flex flex-center fullscreen column bg-black">
+    <h1
+      role="heading"
+      aria-level="1"
+      class="game-title row text-white q-mb-md"
     >
+      Imperial Conflict
+    </h1>
+    <div class="row q-py-sm">
+      <q-btn
+        unelevated
+        rounded
+        color="primary"
+        class="text-weight-regular"
+        padding=".75rem 3rem"
+        label="create an empire"
+        aria-label="create an empire"
+      />
+      <q-btn
+        unelevated
+        rounded
+        color="white"
+        text-color="black"
+        class="q-ml-xs text-weight-regular"
+        padding=".75rem 3rem"
+        label="log in"
+        aria-label="log in"
+      />
+    </div>
   </q-page>
 </template>
 
-<script>
-export default {
-  name: 'PageIndex',
-}
-</script>
+<style lang="sass" scoped>
+.game-title
+  font-size: 2.5rem
+  line-height: 2.5rem
+</style>
