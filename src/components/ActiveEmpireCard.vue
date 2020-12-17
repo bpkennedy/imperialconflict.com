c1d9ce034105857697e5107dcc908a8ca578399e
Brian Kennedy <bpkennedy@gmail.com>
2020-12-17 16:54:43 -0600

    Extract collapse as ExpandableText and update ActiveEmpireCard with Card

--- src/components/ActiveEmpireCard.vue
+++ src/components/ActiveEmpireCard.vue
@@ -4,6 +4,10 @@ import { SELECTED_CURRENT_EMPIRE_ACTION } from '../store'
 export default {
   components: {
     SafeImage: () => import('./SafeImage.vue'),
+    Card: () => import('./Card.vue'),
+    CardBody: () => import('./CardBody.vue'),
+    CardHeader: () => import('./CardHeader.vue'),
+    ExpandableText: () => import('./ExpandableText.vue'),
   },
   props: {
     activeEmpire: {
@@ -14,41 +18,21 @@ export default {
   data: () => ({
     isGoodAvatar: true,
     isGoodEmpireImage: true,
-    descriptionIsOverflowing: false,
-    expanded: false,
   }),
   methods: {
     selectEmpire() {
       this.$store.dispatch(SELECTED_CURRENT_EMPIRE_ACTION, this.activeEmpire.id)
     },
-    checkIfOverflowing() {
-      if(this.$refs[this.descriptionRef].clientHeight < this.$refs[this.descriptionRef].scrollHeight) {
-        this.descriptionIsOverflowing = true
-      }
-    },
-  },
-  computed: {
-    descriptionRef() {
-      return `empire-${this.activeEmpire.id}description`
-    },
-  },
-  mounted() {
-    setTimeout(() => {  // the dom will sometimes update faster than vue has time to check dom heights
-      this.checkIfOverflowing()
-    }, 100)
   },
 }
 </script>
 
 <template>
-  <q-card
-    tabindex="0"
-    class="column cursor-pointer active-empire no-active-hover"
+  <card
     @click="selectEmpire"
-    @keydown.enter="selectEmpire"
-    v-ripple
+    class-list="primary-outline"
   >
-    <q-item>
+    <card-header>
       <q-item-section
         v-if="activeEmpire.avatar_url && isGoodAvatar"
         avatar
@@ -58,7 +42,7 @@ export default {
           :alt="`${activeEmpire.name} Image`"
           @validated="isGoodAvatar = true"
           @invalidated="isGoodAvatar = false"
-          class="empire-image"
+          class="avatar-image"
         />
       </q-item-section>
 
@@ -68,63 +52,31 @@ export default {
           Family #{{ activeEmpire.family_id }}
         </q-item-label>
       </q-item-section>
-    </q-item>
+    </card-header>
 
-    <safe-image
-      v-if="isGoodEmpireImage"
-      :src="activeEmpire.galaxy.map_image_path"
-      :alt="`${activeEmpire.name} Galaxy Image`"
-      @validated="isGoodEmpireImage = true"
-      @invalidated="isGoodEmpireImage = false"
-      class="empire-galaxy-image"
-    />
-
-    <q-card-section>
-      <div
-        :ref="`empire-${activeEmpire.id}description`"
-        class="q-mb-md ellipsis-3-lines line-6-clamp"
-        :class="{'no-ellipsis': expanded }"
-      >
-        {{ activeEmpire.galaxy.description }}
-      </div>
-      <q-btn
-        v-if="descriptionIsOverflowing"
-        color="white"
-        flat
-        dense
-        :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-        @click.stop="expanded = !expanded"
-        :label="expanded ? 'COLLAPSE' : 'EXPAND'"
-        class="absolute-bottom full-width no-ripple text-body2"
-      />
-    </q-card-section>
+    <card-body>
+      <template #image>
+        <safe-image
+          v-if="isGoodEmpireImage"
+          :src="activeEmpire.galaxy.map_image_path"
+          :alt="`${activeEmpire.name} Galaxy Image`"
+          @validated="isGoodEmpireImage = true"
+          @invalidated="isGoodEmpireImage = false"
+          class="full-width"
+        />
+      </template>
+      <template #content>
+        <expandable-text
+          :unique-ref="`empire-${activeEmpire.id}description`"
+          :content="activeEmpire.galaxy.description"
+        />
+      </template>
+    </card-body>
 
     <q-separator />
 
     <q-card-section class="text-uppercase text-weight-medium">
       Enter {{ activeEmpire.galaxy.name }}
     </q-card-section>
-  </q-card>
+  </card>
 </template>
-
-<style lang="sass" scoped>
-.empire-image
-  width: 2.5rem
-  height: 2.5rem
-  max-width: 2.5rem
-  border: 1px solid $dark
-  border-radius: 50%
-  overflow: hidden
-.empire-galaxy-image
-  width: 100%
-.active-empire
-  &:focus
-    outline: 0
-  &:focus-visible
-    outline: 1px solid $primary
-.line-6-clamp
-  -webkit-line-clamp: 6
-.no-ellipsis
-  -webkit-line-clamp: initial
-  -webkit-box-orient: initial
-</style>
---
+++ src/components/ExpandableText.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  props: {
+    uniqueRef: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    minimumLines: {
+      type: Number,
+      default: 6,
+    },
+  },
+  data: () => ({
+    descriptionIsOverflowing: false,
+    expanded: false,
+  }),
+  methods: {
+    checkIfOverflowing() {
+      if(this.$refs[this.uniqueRef].clientHeight < this.$refs[this.uniqueRef].scrollHeight) {
+        this.descriptionIsOverflowing = true
+      }
+    },
+  },
+  mounted() {
+    setTimeout(() => {  // the dom will sometimes update faster than vue has time to check dom heights
+      this.checkIfOverflowing()
+    }, 100)
+  },
+}
+</script>
+
+<template>
+  <div>
+    <div
+      :ref="uniqueRef"
+      class="q-mb-md ellipsis-3-lines"
+      :class="{'no-ellipsis': expanded }"
+      :style="{'-webkit-line-clamp': minimumLines}"
+    >
+      {{ content }}
+    </div>
+    <q-btn
+      v-if="descriptionIsOverflowing"
+      color="white"
+      flat
+      dense
+      :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+      @click.stop="expanded = !expanded"
+      :label="expanded ? 'COLLAPSE' : 'EXPAND'"
+      class="absolute-bottom full-width no-ripple text-body2"
+    />
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.no-ellipsis
+  -webkit-line-clamp: initial
+  -webkit-box-orient: initial
+</style>
