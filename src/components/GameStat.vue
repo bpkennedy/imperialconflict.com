887a5fea9adc0430cb91a8179526f82282779bbc
Brian Kennedy <bpkennedy@gmail.com>
2020-12-17 16:57:44 -0600

    Create a GameStat component which displays planet bonuses and resources

---
+++ src/components/GameStat.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  components: {
+    SafeImage: () => import('./SafeImage.vue'),
+  },
+  props: {
+    iconUrl: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="row col-12">
+    <safe-image
+      v-if="iconUrl"
+      :src="iconUrl"
+      class="icon-image"
+    />
+    <div
+      v-if="$slots.default"
+      class="q-pl-xs"
+    >
+      <slot />
+    </div>
+    <div
+      v-if="$slots.labelValue"
+      class="q-pl-xs"
+    >
+      <slot name="labelValue" />
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.icon-image
+  width: 1rem
+  height: 1rem
+</style>
